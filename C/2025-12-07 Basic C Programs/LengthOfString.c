ccec9a91e31a758d441393f260cfda8612a02ec9
TiredTiddle <Johnny89762@gmail.com>
2025-12-07 13:26:12 +1100

    length of string program I was supposed to finish on 2025-12-05

---
+++ C/2025-12-07 Basic C Programs/LengthOfString.c
@@ -0,0 +1,24 @@
+/*
+ * Name: LengthOfString.c
+ * Author: TiredTiddles
+ * Date: 2025-12-05
+ * 
+ * Description:
+ * Asks for user input, then outputs the length of the inputted string.
+ */
+
+#include <stdio.h>
+
+int main(void) {
+    int len = 0;
+
+    printf("Enter a message: ");
+
+    while (getchar() != '\n') {
+        len++;
+    }
+
+    printf("Your message was %d character(s) long.\n", len);
+
+    return 0; 
+}
